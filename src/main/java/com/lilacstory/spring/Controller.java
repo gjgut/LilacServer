c3b3e40bfda2735c25032c98e7fd20b4bbbc104e
gjgut <gjgustjd70@gmail.com>
2020-12-17 01:47:02 +0900

    commit all files

--- src/main/java/com/lilacstory/spring/Controller.java
+++ src/main/java/com/lilacstory/spring/Controller.java
@@ -1,5 +1,15 @@
 package com.lilacstory.spring;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class Controller {
+
+    @GetMapping("/test")
+    public String hello()
+    {
+        return "Hello World!";
+    }
 }
+
